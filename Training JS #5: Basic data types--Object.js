3d927f45c0b0cd558f15abd4b5577fc7d5296229
Kev <105522526+KevoxChrist@users.noreply.github.com>
2025-08-17 15:40:35 -0400

    Create Training JS #5: Basic data types--Object.js

---
+++ Training JS #5: Basic data types--Object.js
@@ -0,0 +1,18 @@
+// Task
+// Give you a function animal, accept 1 parameter:obj like this:
+// {name:"dog",legs:4,color:"white"}
+
+// and return a string like this:
+// "This white dog has 4 legs."
+
+
+
+
+function animal(obj){
+  
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
+
+
+//MY THOUGHTS
+// I liked this challenge because it strengthened my muscles for accessing a property in an object and it's value. I found this problem within a JavaScript Training playlist which I found to be helpful!
